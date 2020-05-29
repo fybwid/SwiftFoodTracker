d4e4aa289c371f6ec641da7f5115d8cccf5fcc24
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-05-29 13:48:13 +0700

    Initial Commit

---
+++ SwiftFoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftFoodTracker
+//
+//  Created by Yosef Benny Widyokarsono on 29/05/20.
+//  Copyright © 2020 Yosef Benny Widyokarsono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
